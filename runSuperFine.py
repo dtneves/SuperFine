bd2494d75c1c322fbfb8ec4cad580b3c4fc3e54b
dtn <dneves@di.uminho.pt>
2016-11-12 17:50:37 +0000

    The reconciler option supports maximum likelihood methods, namely 'fml' (FastTree) and 'rml' (RAxML).

--- runSuperFine.py
+++ runSuperFine.py
@@ -2,77 +2,73 @@
 
 ###########################################################################
 ##    Copyright 2010 Rahul Suri and Tandy Warnow.
-##    This file is part of SuperFine.
+##    This file is part of superfine.
 ##
-##    SuperFine is free software: you can redistribute it and/or modify
+##    superfine is free software: you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
 ##    the Free Software Foundation, either version 3 of the License, or
 ##    (at your option) any later version.
 ##
-##    SuperFine is distributed in the hope that it will be useful,
+##    superfine is distributed in the hope that it will be useful,
 ##    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##    GNU General Public License for more details.
 ##
 ##    You should have received a copy of the GNU General Public License
-##    along with SuperFine.  If not, see <http://www.gnu.org/licenses/>.
+##    along with superfine.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
+########################################################################################################################
+#   Copyright 2016 Diogo Telmo Neves and Tandy Warnow.                                                                 #
+#   This file is part of superfine and was adapted from the baseline implementation (see above).                       #
+#                                                                                                                      #
+#   Besides some refactoring, features to support running maximum likelihood (ML) analyses were added.                 #
+#                                                                                                                      #
+#   The license is exactly the same of the baseline implementation (see above).                                        #
+########################################################################################################################
+
 from optparse import OptionParser, OptionGroup
-from SuperFine.SuperFine import SuperFine
+from superfine.SuperFine import SuperFine
+
 
 def parseOptions (commandLine = None):
     '''Parse command line for options'''
 
-    desc = ' '.join(['This script runs the SuperFine algorithm on a set of input trees',  
-                     'given in a file, in Newick format.'])
+    desc = "This script runs the superfine algorithm on a set of input trees given in a file, in Newick format."
 
-    parser = OptionParser(usage = "usage: %prog [options] input_trees_file > output", 
-                          version = "%prog 1.0", 
-                          description = desc)
+    parser = OptionParser(usage = "usage: %prog [options] input_trees_file > output",
+                          version = "%prog 1.0", description = desc)
 
-    parser.set_defaults(reconciler = "qmc",
-                        numIters = 100, 
-                        writeData = None)
+    parser.set_defaults(reconciler = "qmc", numIters = 100, writeData = None)
 
-    group4InfoString = ' '.join(["These options enable selection of the supertree", 
-                                 "algorithm to be used as a subroutine within", 
-                                 "SuperFine for resolving polytomies.  The 'qmc'",
-                                 "option requires that Quartets MaxCut be", 
-                                 "installed; the 'gmrp' (greedy consensus of MP", 
-                                 "trees) and 'rmrp' (random MP tree) options", 
-                                 "require that PAUP* be installed.  Note that", 
-                                 "the selected subroutine's binary must be in", 
-                                 "the system's executable search path."])
+    group4InfoString = "These options enable selection of the supertree algorithm " \
+                       "to be used as a subroutine within superfine for resolving polytomies.  " \
+                       "The 'qmc' option requires that Quartets MaxCut be installed; " \
+                       "the 'gmrp' (greedy consensus of MP trees) and 'rmrp' (random MP tree) options " \
+                       "require that PAUP* be installed; " \
+                       "the 'fml' option requires that FastTree be installed; and " \
+                       "the 'rml' option requires that RAxML be installed.  " \
+                       "Note that the selected subroutine's binary must be in the system's executable search path."
+    group4 = OptionGroup(parser, "Quartet Tree Reconciliation Options".upper(), group4InfoString)
+    group4.add_option("-r", "--reconcile", choices=("qmc", "gmrp", "rmrp", "fml", "rml"),
+                      dest="reconciler", metavar="ALG",
+                      help="use ALG to reconcile relabeled trees, "
+                           "where ALG is one of {qmc, gmrp, rmrp} [default: %default]")
+    group4.add_option("-n", "--numIters", type="int", dest="numIters", metavar="N",
+                      help="use N ratchet iterations when resolving with MRP [default: %default]")
+    parser.add_option_group(group4)
 
-    group5InfoString = ' '.join(["This option causes output of both the final", 
-                                 "SuperFine tree and the corresponding SCM tree", 
+    group5InfoString = ' '.join(["This option causes output of both the final",
+                                 "superfine tree and the corresponding SCM tree",
                                  "to be written to disk.  Both are written to", 
                                  "the directory containing the source trees", 
                                  "file, and the specified suffix is appended to", 
                                  "each one's name."])
-
-    group4 = OptionGroup(parser, "Quartet Tree Reconciliation Options".upper(), group4InfoString)
     group5 = OptionGroup(parser, "Data Output Options".upper(), group5InfoString)
-
-    group4.add_option("-r", "--reconcile", choices = ("qmc", "gmrp", "rmrp"), 
-                      dest = "reconciler", metavar = "ALG", 
-                      help = "use ALG to reconcile relabeled trees, "
-                             "where ALG is one of {qmc, gmrp, rmrp} "
-                             "[default: %default]")
-    group4.add_option("-n", "--numIters", type = "int", 
-                      dest = "numIters", metavar = "N", 
-                      help = "use N ratchet iterations when resolving with MRP "
-                             "[default: %default]")
-
-    group5.add_option("-w", "--write", dest = "writeData", metavar = "SUFFIX",  
+    group5.add_option("-w", "--write", dest = "writeData", metavar = "SUFFIX",
                       help = "write merger tree and final tree to disk in same "
                              "directory as source trees file, append .SUFFIX "
-                             "to written file names "
-                             "[default: %default]")
-
-
-    parser.add_option_group(group4)
+                             "to written file names [default: %default]")
     parser.add_option_group(group5)
 
     if commandLine:
