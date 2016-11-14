66da58f9ce09861cda91784f9656abfa1bc80811
dtn <dneves@di.uminho.pt>
2016-11-14 01:37:14 +0000

    Fix documentation.

--- superfine/logger.py
+++ superfine/logger.py
@@ -2,20 +2,20 @@
 
 ###########################################################################
 ##    Copyright 2010 Rahul Suri and Tandy Warnow.
-##    This file is part of superfine.
+##    This file is part of SuperFine.
 ##
-##    superfine is free software: you can redistribute it and/or modify
+##    SuperFine is free software: you can redistribute it and/or modify
 ##    it under the terms of the GNU General Public License as published by
 ##    the Free Software Foundation, either version 3 of the License, or
 ##    (at your option) any later version.
 ##
-##    superfine is distributed in the hope that it will be useful,
+##    SuperFine is distributed in the hope that it will be useful,
 ##    but WITHOUT ANY WARRANTY; without even the implied warranty of
 ##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##    GNU General Public License for more details.
 ##
 ##    You should have received a copy of the GNU General Public License
-##    along with superfine.  If not, see <http://www.gnu.org/licenses/>.
+##    along with SuperFine.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
 import sys
